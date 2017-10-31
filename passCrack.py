f4d7317c2c8c61dc7a14303098130fe9de8cfb34
dingaaling <jending12@gmail.com>
2017-10-31 12:10:58 -0400

    Takes in sweetwords, clusters roots, chooses root with max variations, writes index to output file

--- passCrack.py
+++ passCrack.py
@@ -23,6 +23,18 @@ def read_password_file(filename):
 
     return pw_list
 
+# Write password choice index to output file
+def write_passwordchoice_file(filename, password_list):
+    # Open file (different based on Python version) in write mode
+    if sys.version_info[0] == 3:
+        file = open(filename,"w",errors='ignore')
+    else:
+        file = open(filename,"w")
+
+    output = ", ".join(password_list)
+    file.write(output)
+    file.close()
+
 def cluster(sweetwords_list, n):
 
     sweetwords_dist = np.zeros(n)
@@ -31,12 +43,22 @@ def cluster(sweetwords_list, n):
 
     affprop = sklearn.cluster.AffinityPropagation(affinity="precomputed", damping=0.5)
     affprop.fit(similarity)
+    roots = dict()
+
     for cluster_id in np.unique(affprop.labels_):
+
         exemplar = sweetwords_list[0][affprop.cluster_centers_indices_[cluster_id]]
         cluster = np.unique(sweetwords_list[0][np.nonzero(affprop.labels_==cluster_id)])
         cluster_str = ", ".join(cluster)
         print(" - *%s:* %s" % (exemplar, cluster_str))
+        roots[exemplar] = cluster_str
 
+    return roots
+
+def passSelect(roots_dict):
+
+    maxcount = max(len(v) for v in roots_dict.values())
+    return [k for k, v in roots_dict.items() if len(v) == maxcount]
 
 # Define runtime call
 def main():
@@ -50,16 +72,24 @@ def main():
     input_file = args[3]
 
 	# store input passwords
-    password_list = np.zeros(m)
+    password_list = []
     sweetwords = read_password_file(input_file)
 
+    filename = "password_choice.txt"
 	# Identify password for each set of sweetwords
     for row in range(0,m):
+
         sweetwords_list = []
         sep = ','
         sweetwords_list.append(sweetwords[row].split(sep,n))
-        print("Row Number: ", row)
-        cluster(sweetwords_list, n)
+
+        roots = cluster(sweetwords_list, n)
+        password_choice = "".join(str(x) for x in passSelect(roots))
+        print("Password #",row, ": ", password_choice)
+        password_choice_ind = sweetwords_list[0].index(password_choice)
+        password_list.append(str(password_choice_ind))
+
+    write_passwordchoice_file(filename, password_list)
 
 # Principal function call
 main()
