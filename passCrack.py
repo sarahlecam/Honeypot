98b35532bfdcae915c4977372330d0cdd1200838
Luke Ahn <ahnnuga@gmail.com>
2017-11-04 00:47:56 -0400

    character switches detected

--- passCrack.py
+++ passCrack.py
@@ -43,7 +43,7 @@ def calculate_entropy(input):
         else:
             score += 11
     # if the password is made up entirely of characters
-    elif len([c for c in input if c.isalpha()]) == len(input):
+    elif len([c for c in input if (c.isalpha() and c.islower())]) == len(input):
         score += 10
 
     else:
@@ -53,6 +53,19 @@ def calculate_entropy(input):
     if len([c for c in input if c in specialChars]) >= len(input)*0.3:
         score -= 30
 
+    capitalizationChanges = 0
+    for idx,val in enumerate(input[0:-1]):
+        if input[idx].isalpha() and input[idx+1].isalpha():
+            if input[idx].islower() != input[idx+1].islower():
+                capitalizationChanges += 1
+        elif input[idx].isalpha() and input[idx+1].isdigit():
+            capitalizationChanges += 1
+        elif input[idx].isdigit() and input[idx+1].isalpha():
+            capitalizationChanges += 1
+    if capitalizationChanges>1:
+        score -=10
+    elif capitalizationChanges>3:
+        score -=25
     print(input,"score",score)
     return score
 
